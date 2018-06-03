f49799e60b77624e60ad6e6e739526506588b980
Yusuke Miyazaki <ysk1180@gmail.com>
2018-06-03 13:58:26 +0900

    利用規約・プライバシーポリシー設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'terms', to: 'terms#index', as: :terms
+  get 'privacy', to: 'privacies#index', as: :privacy
   get '/confirm/:id', to: 'posts#confirm', as: :confirm
   resources :posts, only: [:create, :show, :edit]
   root to: "posts#create"
